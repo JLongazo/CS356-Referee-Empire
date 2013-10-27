5a11fd05235d0a9c402d9c5f4dd24028d5ace576
rkeshishians <rkeshishians@csupomona.edu>
2013-10-27 14:15:53 -0700

    Button to RButton
    Button Changed to RButton Button should be deleted.

---
+++ src/com/example/cs356/RButton.java
@@ -0,0 +1,44 @@
+package com.example.refereebuttons;
+
+import android.graphics.Path.Direction;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class RButton {
+	private String name;
+	private ImageView image;
+	private int gridLayout;
+	
+	public RButton(){
+		
+	}
+	public void onTap(){
+		
+	}
+	public void onHold(){	
+	}
+	public void onSwipe(Direction dir){
+		
+	}
+	public void setName(String n){
+		name = n;		
+	}
+	public void setImage(ImageView i){
+		image = i;		
+	}
+	public void setGrid(int g){
+		gridLayout = g;		
+	}
+	public String getName(){
+		return name;		
+	}
+	public ImageView getImage(){
+		return image;		
+	}
+	public int getGrid(){
+		return gridLayout;		
+	}
+	
+
+}
