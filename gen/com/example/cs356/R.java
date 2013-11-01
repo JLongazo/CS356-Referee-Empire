b9886f96990189d6823da20e895d31602130c771
markit251 <MarkyMark@b98-wc1-auth-134-71-200-117.wlan.csupomona.edu>
2013-10-31 21:46:36 -0700

    WhistleButton Testing

--- gen/com/example/cs356/R.java
+++ gen/com/example/cs356/R.java
@@ -11,11 +11,11 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f060000;
-        public static final int blue=0x7f060002;
-        public static final int green=0x7f060003;
-        public static final int red=0x7f060001;
-        public static final int white=0x7f060004;
+        public static final int black=0x7f070000;
+        public static final int blue=0x7f070002;
+        public static final int green=0x7f070003;
+        public static final int red=0x7f070001;
+        public static final int white=0x7f070004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -24,58 +24,69 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int settings=0x7f020002;
+        public static final int gear=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int roundedbutton=0x7f020003;
+        public static final int settings=0x7f020004;
+        public static final int whistle=0x7f020005;
     }
     public static final class id {
-        public static final int Bbuttons=0x7f09000c;
-        public static final int MainLayout=0x7f090006;
-        public static final int Menus=0x7f090007;
-        public static final int Scores=0x7f09000a;
-        public static final int Tbuttons=0x7f09000b;
-        public static final int Teams=0x7f090009;
-        public static final int action_settings=0x7f09000d;
-        public static final int button1=0x7f090000;
-        public static final int button2=0x7f090001;
-        public static final int button3=0x7f090002;
-        public static final int button4=0x7f090003;
-        public static final int button5=0x7f090005;
-        public static final int button6=0x7f090004;
-        public static final int textView1=0x7f090008;
+        public static final int Bbuttons=0x7f0a000e;
+        public static final int MainLayout=0x7f0a0006;
+        public static final int Menus=0x7f0a0007;
+        public static final int Scores=0x7f0a000c;
+        public static final int Settings=0x7f0a0009;
+        public static final int Tbuttons=0x7f0a000d;
+        public static final int Teams=0x7f0a000b;
+        public static final int Whistle=0x7f0a0008;
+        public static final int action_settings=0x7f0a000f;
+        public static final int button1=0x7f0a0000;
+        public static final int button2=0x7f0a0001;
+        public static final int button3=0x7f0a0002;
+        public static final int button4=0x7f0a0003;
+        public static final int button5=0x7f0a0005;
+        public static final int button6=0x7f0a0004;
+        public static final int textView1=0x7f0a000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_scoreboard_ui=0x7f030001;
-        public static final int activity_test=0x7f030002;
+        public static final int activity_dummy_tester=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_scoreboard_ui=0x7f030002;
+        public static final int activity_test=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int scoreboard1=0x7f080001;
-        public static final int scoreboard_ui=0x7f080002;
-        public static final int test=0x7f080003;
+        public static final int dummy_tester=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int scoreboard1=0x7f090002;
+        public static final int scoreboard_ui=0x7f090003;
+        public static final int test=0x7f090004;
+    }
+    public static final class raw {
+        public static final int whistle=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int createscoreboard=0x7f050003;
-        public static final int createtournament=0x7f050009;
-        public static final int hello_world=0x7f050002;
-        public static final int option=0x7f05000a;
-        public static final int resumegame=0x7f050006;
-        public static final int scoreboard=0x7f05000c;
-        public static final int selectscoreboard=0x7f050007;
-        public static final int testbuttonpress=0x7f05000b;
-        public static final int testing=0x7f050005;
-        public static final int title_activity_scoreboard1=0x7f05000e;
-        public static final int title_activity_scoreboard_ui=0x7f05000f;
-        public static final int title_activity_test=0x7f05000d;
-        public static final int viewsavedgames=0x7f050004;
-        public static final int viewscores=0x7f050008;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int createscoreboard=0x7f060003;
+        public static final int createtournament=0x7f060009;
+        public static final int hello_world=0x7f060002;
+        public static final int option=0x7f06000a;
+        public static final int resumegame=0x7f060006;
+        public static final int scoreboard=0x7f06000c;
+        public static final int selectscoreboard=0x7f060007;
+        public static final int testbuttonpress=0x7f06000b;
+        public static final int testing=0x7f060005;
+        public static final int title_activity_dummy_tester=0x7f060010;
+        public static final int title_activity_scoreboard1=0x7f06000e;
+        public static final int title_activity_scoreboard_ui=0x7f06000f;
+        public static final int title_activity_test=0x7f06000d;
+        public static final int viewsavedgames=0x7f060004;
+        public static final int viewscores=0x7f060008;
     }
     public static final class style {
         /** 
@@ -99,11 +110,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int test=0x7f070002;
+        public static final int AppTheme=0x7f080001;
+        public static final int test=0x7f080002;
     }
 }
